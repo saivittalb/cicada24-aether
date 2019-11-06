bbf3f9ce1c587ae7bfc37c2a2bcd89b54d79b7de
Sai Vittal B <36305142+saivittalb@users.noreply.github.com>
2019-11-07 05:03:37 +0530

    Added levels

---
+++ level4.js
@@ -0,0 +1,6 @@
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.getMinutes();
+var getlement = dt.getHours()
+if ( getlement == 13 ) {
+    document.getElementById("datetime").innerHTML = "0_0 rick has fixed th3 tyme maxhine {hackerman}" }
+    
